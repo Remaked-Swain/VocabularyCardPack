04877a32351db401de22c5825af6eda1f89929d6
Remaked-Swain <destap@naver.com>
2023-02-20 14:02:19 +0900

    New creation

---
+++ VocabularyCardPack/VocabularyCardPackApp.swift
@@ -0,0 +1,17 @@
+//
+//  VocabularyCardPackApp.swift
+//  VocabularyCardPack
+//
+//  Created by Swain Yun on 2023/02/20.
+//
+
+import SwiftUI
+
+@main
+struct VocabularyCardPackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
